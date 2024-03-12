a1cc9d3c12f2083728eb705f1354fd76da52c4a1
Aayushi <aayushi16dex@gmail.com>
2024-03-12 20:42:52 +0530

    User role and legal right count

--- controllers/admin/a_countController.js
+++ controllers/admin/a_countController.js
@@ -1,13 +1,15 @@
 const User = require('../../models/user/userModel')
 const Expert = require('../../models/legalExpert/expertModel')
 const Organisation = require('../../models/organisation/organisationModel')
+const Section = require('../../models/legalContent/sectionModel')
 
 // Fetch count for admin homepage
 fetchCount = async (req, res) => {
     const userCount = await User.countDocuments({role:'CHILD'});
     const expertCount = await Expert.countDocuments({role:'LEGAL_EXPERT'});
     const organisationCount = await Organisation.countDocuments();
-    res.status(200).json({userCount, expertCount, organisationCount});
+    const legalRightCount = await Section.countDocuments();
+    res.status(200).json({userCount, expertCount, organisationCount, legalRightCount});
 }
 
 module.exports = {
--- controllers/admin/a_userController.js
+++ controllers/admin/a_userController.js
@@ -1,9 +1,10 @@
-const User = require('../../models/user/userModel')
+const User = require('../../models/user/userModel');
+const XUser = require('../../utils/constants/XUser');
 
 // Fetch list of all User
 fetchUserList = async (req, res) => {
     const [userList, totalCount] = await Promise.all([
-        User.find()
+        User.find({role: XUser.ROLE_CHILD})
         .select('firstName lastName email displayPicture joinedOn'),
         User.countDocuments(),
     ]);
@@ -18,7 +19,7 @@ fetchUser = async (req, res) => {
         const user = await User.findById(userId);
 
         if (!user) {
-            return res.status(404).json({ error: 'user not found' });
+            return res.status(404).json({ error: 'User not found' });
         }
 
         res.status(200).json(user);
